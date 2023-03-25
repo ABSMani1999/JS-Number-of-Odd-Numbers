f318077494e69471c7e93c33099c284411563165
Balasubramanian1999 <absmani1999@gmail.com>
2023-03-25 17:06:55 +0530

    Update

--- main.js
+++ main.js
@@ -1,4 +1,20 @@
-//odd number & count & function	
+
+ 
+ //odd Number
+ 
+ var n=[1,2,3,4,5,6,7,8,9,10];
+ 
+ for (a=0;a<n.length;++a) {
+	 
+	if (n[a]%2!=0){
+		document.write(n[a]);
+		document.write("<br>")
+	} 
+ }
+ 
+ 
+ 
+ //odd number & count & function	
 	
 var n=[12,24,23,52,79,15,46];
 
@@ -12,4 +28,3 @@ function odd (n){
  }return count;		
 }
  document.write ("Number of odd: "+(odd(n)));
-
